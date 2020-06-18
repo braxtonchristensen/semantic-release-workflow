6b894808d5b0ca5705174ffa60dff5c358ebd524
Braxton M. Christensen <braxton.christensen@gmail.com>
2020-06-18 14:19:39 -0600

    fix(format): semantic needs to commit
    test semantic

--- packages/strings/src/index.ts
+++ packages/strings/src/index.ts
@@ -5,7 +5,11 @@ export const concat = curry((a: string, b: string) => {
   return a + b;
 });
 
-export const reverse = (str: string) => str.split('').reverse().join('');
+export const reverse = (str: string) =>
+  str
+    .split('')
+    .reverse()
+    .join('');
 
 export const sumStringNums = curry((a: string, b: string) => {
   const numberA = Number(a);
