f9b786515370ca184e79a7fa6bdc8998a9cbfc19
Braxton M. Christensen <braxton.christensen@gmail.com>
2020-06-18 10:13:36 -0600

    feat(strings): add strings lib
    - add strings lib

---
+++ packages/numbers/src/index.ts
@@ -0,0 +1,15 @@
+import { curry } from 'ramda';
+
+export const sum = curry((a: number, b: number) => {
+  if ('development' === process.env.NODE_ENV) {
+    console.log('boop');
+  }
+  return a + b;
+});
+
+export const diff = curry((a: number, b: number) => {
+  if ('development' === process.env.NODE_ENV) {
+    console.log('boop');
+  }
+  return a - b;
+});
