43a87818abd01d043f7083df1b3dbbce917c6f8c
Braxton M. Christensen <braxton.christensen@gmail.com>
2020-06-18 08:57:25 -0600

    test(curry): test curry
    check curry is working properly

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,10 +1,20 @@
 import { sum, diff } from '../src';
 
-describe('blah', () => {
-  it('sum', () => {
+describe('sum', () => {
+  it('should take 2 args at once', () => {
     expect(sum(1, 1)).toEqual(2);
   });
-  it('diff', () => {
-    expect(diff(1, 1)).toEqual(0);
+  it('should take 1 arg at a time', () => {
+    expect(sum(1)(1)).toEqual(2);
   });
 });
+
+describe('diff', () => {
+  it('should take 2 args at once', () => {
+    expect(diff(1, 1)).toEqual(0);
+  });
+  it('should take 1 arg at a time', () => {
+    expect(diff(1)(1)).toEqual(0);
+  });
+});
+
